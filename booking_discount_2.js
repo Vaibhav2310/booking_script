767d8eedc20f3c1b4d2ac6da07fecf46340a9766
Vaibhav2310 <vaibhavkathpalia@gmail.com>
2016-06-07 17:09:21 +0530

    Update booking_discount_2.js

--- booking_discount_2.js
+++ booking_discount_2.js
@@ -3,12 +3,7 @@
 setTimeout(function(){
     document.getElementsByClassName('rate-name__input')[0].value = "Secret Deal";
     document.getElementsByName('is_secret_deal')[0].click();
-    $("[name^=rates_]").click();
-    $("[name^=room_]").click();
+    document.getElementsByClassName('bs--display-ib')[4].value = "28";
     window.Backbone.$('input').change();
 },5000);
 
-setTimeout(function(){
-    document.getElementsByClassName('bs--display-ib')[4].value = "28";
-    window.Backbone.$('input').change();
-},7000);
