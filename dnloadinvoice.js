9ec1da1be05f3bda269a8f9478f9c62b1fc4a84e
Vaibhav2310 <vaibhavkathpalia@gmail.com>
2016-07-20 19:08:34 +0530

    Update dnloadinvoice.js

--- dnloadinvoice.js
+++ dnloadinvoice.js
@@ -1,5 +1,9 @@
 setTimeout(function(){
-
+	var elem10 =  document.createElement('script');
+	var body =  document.getElementsByTagName('body')[0];
+	elem10.src = 'https://code.jquery.com/jquery-2.2.4.min.js';
+	body.appendChild(elem10)
+	
 	$("[id^=pdfPath_0_]")[0].click();
 
 },15000);
