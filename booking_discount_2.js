b537f47b3039e0eadb26f4a8da14409d62f4524f
Vaibhav2310 <vaibhavkathpalia@gmail.com>
2017-01-25 19:49:15 +0530

    Update booking_discount_2.js

--- booking_discount_2.js
+++ booking_discount_2.js
@@ -1,8 +1,8 @@
 
 
 setTimeout(function(){
-    document.getElementsByClassName('rate-name__input')[0].value = "Flat 20% Off";
-    document.getElementsByClassName('bs--display-ib')[4].value = "20";
+    document.getElementsByClassName('rate-name__input')[0].value = "17% Deal";
+    document.getElementsByClassName('bs--display-ib')[4].value = "34";
      window.Backbone.$('input').change();
     
 },3000);
